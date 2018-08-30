ce4c17d0d1df23b1fadb42534dc1ada1d420e1b9
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-08-30 11:26:14 +0200

    Add Team class

---
+++ src/main/java/com/codecool/quest/store/model/Team.java
@@ -0,0 +1,14 @@
+package com.codecool.quest.store.model;
+
+public class Team extends Item {
+
+    private int classId;
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+}
