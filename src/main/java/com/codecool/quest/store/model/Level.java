83efd122b75374cf3cef3cac8f0cfe37092a57fb
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-08-30 11:25:56 +0200

    Add Level class

---
+++ src/main/java/com/codecool/quest/store/model/Level.java
@@ -0,0 +1,23 @@
+package com.codecool.quest.store.model;
+
+public class Level extends Item {
+
+    private int startValue;
+    private int endValue;
+
+    public int getStartValue() {
+        return startValue;
+    }
+
+    public void setStartValue(int startValue) {
+        this.startValue = startValue;
+    }
+
+    public int getEndValue() {
+        return endValue;
+    }
+
+    public void setEndValue(int endValue) {
+        this.endValue = endValue;
+    }
+}
