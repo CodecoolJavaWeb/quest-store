2998371036d93dc823120bf36ec0adf3cb5e4432
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-08-30 11:25:35 +0200

    Add Artifact class

---
+++ src/main/java/com/codecool/quest/store/model/Artifact.java
@@ -0,0 +1,23 @@
+package com.codecool.quest.store.model;
+
+public class Artifact extends Item {
+
+    private String description;
+    private int price;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
