a26eb781aaa4b9cc848390c80770427de63b4d51
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-08-30 11:13:56 +0200

    Add User class

---
+++ src/main/java/com/codecool/quest/store/model/User.java
@@ -0,0 +1,50 @@
+package com.codecool.quest.store.model;
+
+public class User extends TransferObject {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private int classId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+}
