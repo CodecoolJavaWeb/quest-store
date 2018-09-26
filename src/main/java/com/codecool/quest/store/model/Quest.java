74c14e88cf75f7a71f223e11150c5438f9d6fbf4
Maziarella <kiebala.magdalena@gmail.com>
2018-09-26 11:12:30 +0200

    Changes in class Codecooler: add fields id, and name, implement getters and setters to added fields, override hashCode() and equals() methods.

--- src/main/java/com/codecool/quest/store/model/Quest.java
+++ src/main/java/com/codecool/quest/store/model/Quest.java
@@ -1,10 +1,29 @@
 package com.codecool.quest.store.model;
 
-public class Quest extends Item {
+public class Quest  {
 
+    private int id;
+    private String name;
     private String description;
     private int value;
     private boolean isExtra;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getDescription() {
         return description;
@@ -29,4 +48,24 @@ public class Quest extends Item {
     public void setExtra(boolean extra) {
         isExtra = extra;
     }
+
+
+
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj instanceof Quest) {
+            Quest other = (Quest) obj;
+            return this.id == other.getId();
+        }
+        return false;
+    }
+
+
 }
