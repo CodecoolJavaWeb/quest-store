d76d11081960f2781188cc75e01db056af9f71a5
Maziarella <kiebala.magdalena@gmail.com>
2018-09-26 11:11:55 +0200

    Changes in class Mentor: add fields id, classId and basicUserData, implement getters and setters to added fields, override hashCode() and equals() methods.

--- src/main/java/com/codecool/quest/store/model/Mentor.java
+++ src/main/java/com/codecool/quest/store/model/Mentor.java
@@ -1,4 +1,47 @@
 package com.codecool.quest.store.model;
 
-public class Mentor extends User {
+public class Mentor {
+    private int id;
+    private int classId;
+    private BasicUserData basicUserData;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    public BasicUserData getBasicUserData() {
+        return basicUserData;
+    }
+
+    public void setBasicUserData(BasicUserData basicUserData) {
+        this.basicUserData = basicUserData;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj instanceof Mentor) {
+            Mentor other = (Mentor) obj;
+            return this.id == other.getId();
+        }
+        return false;
+    }
 }
