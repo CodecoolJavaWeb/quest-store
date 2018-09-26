bbe68045d7c6e75965dba501a178854a9b02ab29
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-09-26 14:44:38 +0200

    Add session DAO interface

---
+++ src/main/java/com/codecool/quest/store/controller/dao/SessionDAO.java
@@ -0,0 +1,8 @@
+package com.codecool.quest.store.controller.dao;
+
+import com.codecool.quest.store.model.Session;
+
+public interface SessionDAO {
+    void addSession(Session session);
+    void removeSessionById(String sessionId);
+}
