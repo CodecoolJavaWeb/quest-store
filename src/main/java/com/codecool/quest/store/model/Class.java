786b72e1ee6a8ec71ad48c7ce87c720b67b9701f
Maziarella <kiebala.magdalena@gmail.com>
2018-09-26 11:06:37 +0200

    In class Class: add fields id and name, implement getters and setters to added fields, override hashCode() and equals() methods.

--- src/main/java/com/codecool/quest/store/model/Class.java
+++ src/main/java/com/codecool/quest/store/model/Class.java
@@ -1,4 +1,39 @@
 package com.codecool.quest.store.model;
 
-public class Class extends Item {
+public class Class  {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj instanceof Class) {
+            Class other = (Class) obj;
+            return this.id == other.getId();
+        }
+        return false;
+    }
 }
