479a4a6ff9773fac939b4220c2a881a463a18f54
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-09-28 19:28:27 +0200

    Add sql script to add sample mentors to databse

---
+++ sql_scripts/db_creation_scripts/add_mentors.sql
@@ -0,0 +1,15 @@
+﻿INSERT INTO basic_user_data 
+(first_name, last_name, email, password) 
+VALUES 
+('Mariusz', 'Chudy', 'm.chudy@codecool.com', 'chudy'),
+('Marian', 'Czech', 'm.czech@codecool.com', 'czech');
+
+INSERT INTO mentors (basic_data_id, class_id) VALUES 
+(
+	(SELECT id FROM basic_user_data WHERE email = 'm.chudy@codecool.com'),
+	(SELECT id FROM classes WHERE name = 'Java')
+),
+(
+	(SELECT id FROM basic_user_data WHERE email = 'm.czech@codecool.com'),
+	(SELECT id FROM classes WHERE name = 'Java')
+);
