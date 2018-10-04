3316b2f4f4395e26d2be6cb3dafedd00bf73b3df
Maziarella <kiebala.magdalena@gmail.com>
2018-10-04 11:48:29 +0200

    Add sql scripts which add teams into base.

---
+++ sql_scripts/db_creation_scripts/add_teams.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO teams (team_name, class_id) VALUES 
+(
+	('team_one'),
+	(SELECT id FROM classes WHERE class_name = 'Programming Basics')
+),
+(
+	('team_two'),
+	(SELECT id FROM classes WHERE class_name = 'OOP with Java')
+);
