45722f8e5d1f11bc1470845f607cc6c2bd460698
Maziarella <kiebala.magdalena@gmail.com>
2018-10-04 11:48:02 +0200

    Add sql scripts which add quests into base.

---
+++ sql_scripts/db_creation_scripts/add_quests.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO quests (quest_name, description, reward, is_Extra) VALUES 
+
+	('Quest_one', 'Description_one', 5, TRUE),
+	('Quest_two', 'Description_two', 2, FALSE),
+	('Quest_three', 'Description_three', 3, FALSE);
+
