82468a658a3e61b99dd1240f8a6d0ec20b7866dc
Tomasz Łukaszyk <el.moue@gmail.com>
2018-10-05 22:57:03 +0200

    Add method to validate session

--- src/main/java/com/codecool/quest/store/controller/helpers/SessionCookieHandler.java
+++ src/main/java/com/codecool/quest/store/controller/helpers/SessionCookieHandler.java
@@ -1,9 +1,18 @@
 package com.codecool.quest.store.controller.helpers;
 
+import com.codecool.quest.store.controller.dao.ConnectionFactory;
+import com.codecool.quest.store.controller.dao.DbSessionDAO;
+import com.codecool.quest.store.controller.dao.SessionDAO;
+import com.codecool.quest.store.model.Session;
+import com.sun.net.httpserver.HttpExchange;
+
 import java.net.HttpCookie;
 import java.util.List;
 
 public class SessionCookieHandler {
+
+    private SessionDAO sessionDAO = new DbSessionDAO(new ConnectionFactory().getConnection());
+    private Session session = null;
 
     public String getSessionIdFromCookie(String cookieStr) {
         String[] cookieStrings = cookieStr.split("; ");
@@ -17,5 +26,17 @@ public class SessionCookieHandler {
             return cookies.get(0).getValue();
         }
         return "";
+    }
+
+    public boolean isSessionValid(HttpExchange httpExchange, AccountType accountType) {
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        String sessionId = getSessionIdFromCookie(cookieStr);
+        session = sessionDAO.getSession(sessionId);
+
+        return session != null && session.getAccountType() == accountType;
+    }
+
+    public Session getSession() {
+        return session;
     }
 }
